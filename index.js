391e9841725195dfe3e371f02076e16aa42b04da
Daniel Njuguna <117805781+kagu-dante@users.noreply.github.com>
2022-12-08 17:22:02 +0300

    Completed

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+const main = document.getElementById("main");
+main.remove();
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+const myName = "Ali";
+newHeader.textContent = `${myName} is the champion`;
+Footer
+© 2022 GitHub, Inc.
